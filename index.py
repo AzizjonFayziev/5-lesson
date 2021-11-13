85e00b9f77c26fb864a510aa489f1a90e54a4131
Fayziev Azizjon <azizdjon75@gmail.com>
2021-11-13 19:02:52 +0500

    забыл текст

---
+++ index.py
@@ -0,0 +1,6 @@
+name = input('Enter your name:')
+surname = input('Enter your surname:')
+
+print('hello, ' + name +  ' '+ surname)
+print(f"Hello, {name} {surname}")
+print('hello, {} {}'. format(name, surname))
